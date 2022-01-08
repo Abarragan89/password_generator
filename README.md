5735302662a164f0239c161db87de779fad31be9
Anthony Barragan <anthony.bar.89@gmail.com>
2022-01-08 13:30:49 -0800

    fully functioning.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Password Generator Starter Code
+
+## How it works:
+* This password generator asks the user how long they would like their password. Then the user selects which types of characters to include in their password. The program then makes a password with the given length and ensures at least one of each character type is included in the password. 
+
+## About the code:
+* This is an object oriented program that uses JavaScript classes. 
--- script.js
+++ script.js
@@ -1,82 +1,111 @@
 // Possible characters for password
-const possibleChars = { 
-  special: ["!","#","$","%","&","'","(",")","*", "+", ",","-", ".", "/", ":", ";","<","=", ">","?","@","[", "\\","]","^","_","`","{","|","}","~"],
+const possibleChars = {
+  special: ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"],
   lowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
   numeric: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
-  uppercase:["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  uppercase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 };
-
+//////////////////HELPER FUNCTIONS//////////////////
 // RandomNumber 
-const randomNumber = function(max) {
+const randomNumber = function (max) {
   let number = Math.floor(Math.random() * max);
   return number;
 }
-
+// Shuffle Array
+const shuffle = function (array) {
+  let currentIndex = array.length, randomIndex;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+/////////////Password Class////////////////////
 //Password Class
 class PasswordObject {
   constructor() {
     this.pwLength = undefined;
     this.characterTypesPicked = [];
-    this.password = "";
+    this.password = [];
+    this.mandatoryChars = [];
   }
+  //Asks length from user
   askLength() {
-    this.pwLength = prompt ("How long do you want your password to be? Must be between 8 to 128 characters");
+    this.pwLength = prompt("How long do you want your password to be? Must be between 8 to 128 characters");
     this.pwLength = parseInt(this.pwLength);
-  
-    if (this.pwLength < 8 || this.pwLength > 12800 || isNaN(this.pwLength)) {
+    //Validate user input
+    if (this.pwLength < 8 || this.pwLength > 128 || isNaN(this.pwLength)) {
       alert("input invalid. Please select a length ");
       this.askLength();
-    } 
-    console.log("password length " + this.pwLength);
+    }
   }
+  //Asks character types from user
   characterTypes() {
     let lowercase = confirm("Would you like to include lowercase letters?");
-      if(lowercase) {
-        this.characterTypesPicked.push("lowercase");
-      }
+    if (lowercase) {
+      this.characterTypesPicked.push("lowercase");
+    }
     let uppercase = confirm("Would you like to include uppercase letters?");
-      if(uppercase) {
-        this.characterTypesPicked.push("uppercase");
-      }
+    if (uppercase) {
+      this.characterTypesPicked.push("uppercase");
+    }
     let numeric = confirm("Would you like to include numeric letters?");
-      if(numeric) {
-        this.characterTypesPicked.push("numeric");
-      }
+    if (numeric) {
+      this.characterTypesPicked.push("numeric");
+    }
     let specialCharacters = confirm("Would you like to include special characters?");
-      if(specialCharacters) {
-        this.characterTypesPicked.push("special");
-      }
+    if (specialCharacters) {
+      this.characterTypesPicked.push("special");
+    }
+    //Validate user input
     if (!lowercase && !uppercase && !numeric && !specialCharacters) {
       alert("You need to accept at least one type of character to include in your password.")
-      characterTypes();
+      this.characterTypes();
     }
-    console.log("Character Types " + this.characterTypesPicked);
   }
+  //Create a random password with given character types
   randomPassword() {
-    if (!this.characterTypesPicked.includes("lowercase")){
+    if (!this.characterTypesPicked.includes("lowercase")) {
       delete possibleChars.lowercase;
+    } else {
+      this.mandatoryChars.push(possibleChars.lowercase[Math.floor(Math.random() * possibleChars.lowercase.length)]);
     }
-    if (!this.characterTypesPicked.includes("uppercase")){
+    if (!this.characterTypesPicked.includes("uppercase")) {
       delete possibleChars.uppercase;
-    } 
-    if (!this.characterTypesPicked.includes("numeric")){
+    } else {
+      this.mandatoryChars.push(possibleChars.uppercase[Math.floor(Math.random() * possibleChars.uppercase.length)])
+    }
+    if (!this.characterTypesPicked.includes("numeric")) {
       delete possibleChars.numeric;
+    } else {
+      this.mandatoryChars.push(possibleChars.numeric[Math.floor(Math.random() * possibleChars.numeric.length)])
     }
-    if (!this.characterTypesPicked.includes("special")){
+    if (!this.characterTypesPicked.includes("special")) {
       delete possibleChars.special;
+    } else {
+      this.mandatoryChars.push(possibleChars.special[Math.floor(Math.random() * possibleChars.special.length)])
     }
-    for (let i = 1; i <= this.pwLength; i++){
+    //loop through the remaining arrays in possibleChars Object
+    for (let i = 1; i <= this.pwLength; i++) {
       let keys = Object.keys(possibleChars);
       let chosenArray = keys[Math.floor(Math.random() * keys.length)];
-        this.password += chosenArray[randomNumber(chosenArray.length)];
+      this.password.push(possibleChars[chosenArray][Math.floor(Math.random() * possibleChars[chosenArray].length)])
     }
-        
+    //Ensure at least one character of each type chosen is in password
+    let temp = this.password.slice(this.mandatoryChars.length);
+    this.password = [...temp, ...this.mandatoryChars];
+    //randomize order of password
+    shuffle(this.password)
+    //display password
+    this.password = this.password.join("");
   }
 }
-
-
-
-//Main code sequence
+///////// Main Code Sequence /////////////////
 const writePassword = function () {
   //Confirm they want to make a password
   let getStarted = confirm("Welcome to the password generator. If you wish to continue and create a secure password, please click 'ok'.")
@@ -84,9 +113,9 @@ const writePassword = function () {
   //prompt user for length of password
   if (getStarted) {
     password.askLength();
-  //prompt user for character types
+    //prompt user for character types
     password.characterTypes();
-    debugger;
+    // debugger;
     password.randomPassword();
   }
   const paragraph = document.getElementById("password");
